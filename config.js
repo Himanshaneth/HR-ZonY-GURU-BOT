070b464236b36f196e869ec744edf5bc953fd9ba
Himanshaneth <132945937+Himanshaneth@users.noreply.github.com>
2023-05-09 01:21:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94770104894', 'GURU', true],
+  ['94770104894', 'himansha', true], 
   [''] 
 ] //Number of owners
 
